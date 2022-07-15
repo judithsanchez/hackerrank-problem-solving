4c2d306775ea09bdc93cf5239f790aa47c1c3a0f
Judith Sanchez <judithv.sanchezc@gmail.com>
2022-07-16 01:34:50 +0200

    Added Electronic Shop.

---
+++ electronics-shop.js
@@ -0,0 +1,20 @@
+function getMoneySpent(keyboards, drives, b) {
+   let sum = []
+
+   for (let i = 0; i < keyboards.length; i++) {
+      for (let j = 0; j < drives.length; j++) {
+         sum.push(keyboards[i] + drives[j])
+      }
+   }
+   sum.sort((a, b) => b - a)
+
+   for (let i = 0; i < sum.length; i++) {
+      if (sum[i] <= b) {
+         return sum[i]
+      }
+   }
+   return -1
+}
+
+console.log(getMoneySpent([40, 60, 50], [8, 5, 12], 60))
+console.log(getMoneySpent([3, 1], [5, 2, 8], 10))
