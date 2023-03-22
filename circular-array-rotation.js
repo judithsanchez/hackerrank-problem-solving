913f909ff52835db2720e6968e1886affbcad15b
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-03-22 16:03:28 -0500

    Finished circular array rotation

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -1,16 +1,10 @@
 function circularArrayRotation(a, k, queries) {
-  const result = [];
-
   while (k > 0) {
     a.unshift(a.pop());
     k -= 1;
   }
-
-  for (let i = 0; i < queries.length; i++) {
-    result.push(queries.indexOf(queries[i]));
-  }
-
+  const result = queries.map((q) => a[q]);
   return result;
 }
 
-circularArrayRotation([3, 4, 5], 2, [1, 2]);
+console.log(circularArrayRotation([3, 4, 5], 2, [1, 2])); // 5 3
