c2037f91b9d16694719587c9cd4020715eab6213
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-05-26 22:02:23 -0500

    Added readme

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,20 +1,18 @@
-
 // Given an array of integers and a positive integer k, determine the number of (i, j) pairs where  i < j and  ar[i]+ar[j]  is divisible by k.
 
 function divisibleSumPairs(n, k, ar) {
-   let counter = 0;
+  let counter = 0;
 
-   ar.sort((a,b) => a-b)
+  ar.sort((a, b) => a - b);
 
-   for (let i = 0; i < ar.length; i++){
-
-      for (let j = i+1; j < ar.length; j++) {
-         if ((ar[i] + ar[j]) % k === 0) {
-            counter++
-         }
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        counter += 1;
       }
-   }
-   return counter
+    }
+  }
+  return counter;
 }
 
-console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
--- sequence-equation.js
+++ sequence-equation.js
@@ -1,6 +1,17 @@
 // https://www.hackerrank.com/challenges/permutation-equation/problem?isFullScreen=true
 
 function permutationEquation(p) {
-  // Write your code here
+  const result = [];
 
+  for (let i = 1; i <= p.length; i++) {
+    const a = p.indexOf(i) + 1;
+    const b = p.indexOf(a) + 1;
+    const c = p.indexOf(b) + 1;
+    result.push(p[c - 1]);
+  }
+
+  return result;
 }
+
+console.log(permutationEquation([5, 2, 1, 3, 4]));
+console.log(permutationEquation([4, 3, 5, 1, 2]));
